3fbf82f0fbddabb7c44b8dd8db422776b5ea9a89
Trips424 <uditini531@gmail.com>
2025-12-03 04:02:34 +0000

    begun login page work

---
+++ lib/login_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Log In",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+
+              TestField(
+                decoration: InputDecoration(
+                  labelText: 'Email or Username',
+                  border: OutlineInputBorder(),
+              )
+              const SizedBox(height: 20),
+
+
+
+
