8d8c1fbec35df343add2b484122f9f237a08a112
Trips424 <uditini531@gmail.com>
2025-12-03 23:10:18 +0000

    created footer

---
+++ lib/footer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Footer extends StatelessWidget{
+  const Footer([super.key]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.grey[200],
+      padding: const EdgeInsets.all(vertical: 40, horizontal: 24),
+      
+    )
+  }
+}
