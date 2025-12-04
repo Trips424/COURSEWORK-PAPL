d1ab9411c14c6bb8a17d00f3cf3f5a6c43337cf5
Trips424 <uditini531@gmail.com>
2025-12-04 23:42:56 +0000

    begun cart page

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/models/cart_item.dart';
 
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+   @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  final cart = CartService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Cart"),
+        backgroundColor: const Color(0xFF4d2963),
+        foregroundColor: Colors.white,
