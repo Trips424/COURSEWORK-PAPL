41b184e6abd7f4458c574319ffd2195a50f1785c
Trips424 <uditini531@gmail.com>
2025-12-04 21:46:24 +0000

    started adding functions for cart management

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -11,3 +11,21 @@ class CartItem {
     this.quantity = 1,
   });
 }
+
+class CartService {
+  static final CartService _instance = CartService._internal();
+  factory CartService() => _instance;
+
+  CartService._internal();
+  final List<CartItem> _items = [];
+
+  void addToCart(String name, String imageUrl, String price) {
+
+    final existing = items.where(item) => item.name == name).toList();
+    if (existing.isNotEmpty) {
+      existing[0].quantity += 1;
+    } else {
+      _items.add(CartItem(name: name, imageUrl: imageUrl, price: price));
+    }
+  }
+}
