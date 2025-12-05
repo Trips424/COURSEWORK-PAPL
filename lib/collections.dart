5ee4ff2ab81e840cdf4d437d8035f866f6470efe
Trips424 <uditini531@gmail.com>
2025-12-05 06:08:05 +0000

    created initial collections page

---
+++ lib/collections.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/top_navbar.dart';
+import 'package:union_shop/footer.dart';
+
+class CollectionsPage extends StatelessWidget {
+  const CollectionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final collections = [
+      {
+        'title': 'Hoodies',
+        'image': 'https://via.placeholder.com/300x300?text=Hoodies'
+      },
+      {
+        'title': 'T-Shirts',
+        'image': 'https://via.placeholder.com/300x300?text=T-Shirts'
+      },
+      {
+        'title': 'Accessories',
+        'image': 'https://via.placeholder.com/300x300?text=Mugs'
+      },
+    ]
+  }
